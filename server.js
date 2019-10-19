5293b28759cc68f5bb40d4067e8e7191962b1665
lrb711 <lucasrbarth@gmail.com>
2019-10-18 23:13:02 -0500

    fix error

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const items = require('./routes/api/items');
 const email = require('./routes/email');
 
 const app = express()
+app.use(cors())
 app.use(bodyParser.json());
 
 //database key
