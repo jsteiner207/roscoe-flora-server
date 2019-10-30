f1ac35c22e298b88618130edad5bc6f89d8dda4d
Jacob Steiner <jtsnwn@mail.umsl.edu>
2019-10-29 21:49:37 -0500

    got rid of profanity

--- server.js
+++ server.js
@@ -1,30 +1,32 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const items = require('./routes/api/items');
-const email = require('./routes/email');
+const items = require("./routes/api/items");
+const email = require("./routes/email");
 
-const app = express()
-app.use(cors())
+const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
 //database key
-const db = require('./keys').mongoURI;
+const db = require("./keys").mongoURI;
 
 // Connect to Mongo
 mongoose
-    .connect(db, {useNewUrlParser : true,  useUnifiedTopology: true })
-    .then(() => console.log("MongoDB Connected..."))
-    .catch((ERR) => console.log(ERR));
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch(ERR => console.log(ERR));
 
-    // Use items
+// Use items
 const port = process.env.PORT || 5000;
 
-app.use('/api/items', items);
-app.use('/email', email);
+app.use("/api/items", items);
+app.use("/email", email);
 
-app.get('/', (req, res) => {res.send('woah bitch woah')});
+app.get("/", (req, res) => {
+  res.send("this is the servers root");
+});
 
- app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => console.log(`Server started on port ${port}`));
