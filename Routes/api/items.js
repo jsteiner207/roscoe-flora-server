af98fb615eb7d3e88d7f87bb73c8d21d6c242a18
Jacob Steiner <jtsnwn@mail.umsl.edu>
2019-11-11 23:13:33 -0600

    added put and data

--- Routes/api/items.js
+++ Routes/api/items.js
@@ -1,69 +1,70 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Item Model
-const Item = require('../../models/item');
+const Item = require("../../models/item");
 
 // @route   GET api/items
 // @desc    All Items
 // @access  Public
 
-router.get('/', (req, res) => {
-    Item.find()
-        .sort({ date: -1})  // 1 is ascending, -1 is descending, we want descending
-        .then(items => res.json(items));
+router.get("/", (req, res) => {
+  Item.find()
+    .sort({ date: -1 }) // 1 is ascending, -1 is descending, we want descending
+    .then(items => res.json(items));
 });
 
 //this is used to search for an appointment id and sends the data for that appointment back to the appointment page
-router.get('/:appointmentid', (req, res) => {
-    console.log(req.params.appointmentid)
-    Item.findOne({appointment_id : req.params.appointmentid})
-        .then(items => res.json(items));
+router.get("/:appointmentid", (req, res) => {
+  console.log(req.params.appointmentid);
+  Item.findOne({ appointment_id: req.params.appointmentid }).then(items =>
+    res.json(items)
+  );
 });
 
-router.put('/:appointmentid', (req, res) => {
-    Item.findOneAndUpdate({appointment_id : req.params.appointmentid}, req.body)
-        .then( () => Item.findOne({appointment_id : req.params.appointmentid})
-            .then(console.log(req.body)));
-    console.log("the dead is done");
-    });
+router.put("/:appointmentid", (req, res) => {
+  Item.findOneAndUpdate(
+    { appointment_id: req.params.appointmentid },
+    req.body
+  ).then(() => res.json({ success: true }));
+});
 
 // @route   POST api/items
 // @desc    Create A Item
 // @access  Public
 
-router.post('/', (req, res) => {
-    const newItem = new Item({
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        email_name: req.body.email_name,
-        phone_number: req.body.phone_number,
-        outfit_changes: req.body.outfit_changes,
-        photoshoot_type: req.body.photoshoot_type,
-        location: req.body.location,
-        appointment_id: req.body.appointment_id
-    });
+router.post("/", (req, res) => {
+  const newItem = new Item({
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email_name: req.body.email_name,
+    phone_number: req.body.phone_number,
+    outfit_changes: req.body.outfit_changes,
+    photoshoot_type: req.body.photoshoot_type,
+    location: req.body.location,
+    address: req.body.address,
+    special_requests: req.body.special_requests,
+    appointment_id: req.body.appointment_id,
+    appointment_date: req.body.appointment_date
+  });
 
-    newItem.save()
-        .then(item => {
-            res.json(item)
-            console.log(item.first_name + " was added to the database")
-            })  
-        .catch(() => console.log("That was some weird shit my guy"));
+  newItem
+    .save()
+    .then(item => {
+      res.json(item);
+      console.log(item.first_name + " was added to the database");
+    })
+    .catch(() => console.log("That was some weird shit my guy"));
 });
 
 // @route   DELETE api/items/:id
 // @desc    Delete A Item
 // @access  Public
 
-router.delete('/:id', (req, res) => {
-    Item.findById(req.params.id)
-        .then(item => item.remove()
-        .then(() => res.json({success: true})))
-        .catch(err => res.status(404).json({success: false}));
+router.delete("/:id", (req, res) => {
+  Item.findById(req.params.id)
+    .then(item => item.remove().then(() => res.json({ success: true })))
+    .catch(err => res.status(404).json({ success: false }));
 });
-
-
-
 
 module.exports = router;
--- models/item.js
+++ models/item.js
@@ -1,36 +1,56 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Create Schema
 const ItemSchema = new Schema({
-    first_name: {
-        type: String,
-        required: false},
-    last_name: {
-        type: String,
-         required: true},
-    email_name: {
-        type: String,
-        required: false},
-    phone_number: {
-        type: String,
-        required: false},
-    outfit_changes: {
-        type: String,
-        required: false},
-    photoshoot_type: {
-        type: String,
-        required: false},
-    location: {
-        type: String,
-        required: false},
-    appointment_id: {
-        type: String,
-        required: false
-    },
-    date: {
-        type: Date,
-        default: Date.now}
+  first_name: {
+    type: String,
+    required: false
+  },
+  last_name: {
+    type: String,
+    required: true
+  },
+  email_name: {
+    type: String,
+    required: false
+  },
+  phone_number: {
+    type: String,
+    required: false
+  },
+  outfit_changes: {
+    type: String,
+    required: false
+  },
+  photoshoot_type: {
+    type: String,
+    required: false
+  },
+  location: {
+    type: String,
+    required: false
+  },
+  address: {
+    type: String,
+    required: false
+  },
+  appointment_id: {
+    type: String,
+    required: false
+  },
+  appointment_date: {
+    type: String,
+    required: false
+  },
+  special_requests: {
+    type: String,
+    required: false
+  },
+  date_scheduled: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-module.exports = Item = mongoose.model('item', ItemSchema);
+module.exports = Item = mongoose.model("item", ItemSchema);
